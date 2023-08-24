c129e0e84ef179d486ecc478cc0b4763fb566bd7
Danskuvv <danielvv@metropolia.fi>
2023-08-24 15:36:00 +0300

    JS funktiot tehtävä 1

---
+++ JS kertaus funktiot/JSF1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+console.log(fruits);
+
+const length = fruits.length;
+console.log(length);
+
+const fruit2 = fruits[2];
+console.log(fruit2);
+
+const fruitlast = fruits[length - 1];
+console.log(fruitlast);
+
+const vegetables = [];
+
+const first = prompt('Anna vihannes');
+vegetables.push(first);
+const second = prompt('Anna vihannes');
+vegetables.push(second);
+const third = prompt('Anna vihannes');
+vegetables.push(third);
+
+console.log(vegetables);
+const vegelenght = vegetables.length;
+console.log(vegelenght);
